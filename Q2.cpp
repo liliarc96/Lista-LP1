23c4def974c9cc140b4278e9dc02105162d42293
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2017-09-17 20:04:27 -0300

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main(){
     float largura, comprimento, area;
 
-    cout<< "Digite a largura do lote em metros: \n";
+    cout<< "Digite a largura do lote em metros: " << endl;
     cin>> largura;
-    cout<< "Digite a comprimento do lote em metros: \n";
+    cout<< "Digite a comprimento do lote em metros: " << endl;
     cin>> comprimento;
 
     area = largura * comprimento;
