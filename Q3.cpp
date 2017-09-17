365e37160583330ac2b943e8db62a61db727d2ac
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2017-09-17 17:52:06 -0300

    Add files via upload

---
+++ Q3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float tempC, tempF;
+
+    cout<< "Digite a temperatura em Fahrenheit: \n";
+    cin>> tempF;
+
+    tempC = (tempF - 32)/1.8;
+
+    cout.precision(2);
+    cout<< "A temperatura convertida eh de " << tempC << " graus Celcius." << endl;
+}
