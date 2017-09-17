c3c89ee190eeb96c07532c2c467f7b897578589e
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2017-09-17 16:44:49 -0300

    Add files via upload

---
+++ Q1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nota1, nota2;
+    float media;
+
+    cout<< "Digite a primeira nota: \n";
+    cin>> nota1;
+    cout<< "Digite a segunda nota: \n";
+    cin>> nota2;
+
+    media = (nota1 + nota2)/2;
+
+    cout<< "A media aritimetica das notas eh: " << media << endl;
+}
