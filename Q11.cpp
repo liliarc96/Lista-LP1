fe50db9af865011df88e65be8f6fd5e0b5fea542
Lilia Cantalice <26139056+liliarc96@users.noreply.github.com>
2017-09-18 11:05:38 -0300

    Add files via upload

---
+++ Q11.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 1;
+
+    for( ; i<= 500; ++i){
+        cout<< i << " Eu nao vou mais jogar aviaozinhos de papel na sala de aula." << endl;
+    }
+}
